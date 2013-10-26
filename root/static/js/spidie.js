2bd97f634eadd9139bacd544950056f94081ca4b
Espen Fjellvær Olsen <espen@mrfjo.org>
2013-10-27 00:07:21 +0200

    Beautify js

--- root/static/js/spidie.js
+++ root/static/js/spidie.js
@@ -1,63 +1,49 @@
 var failed = false;
+
 var blocked = false;
 
-$(document).ready(function(){
-    $("#urlform").submit(
-        function(){
-            console.info("Posting");
-            event.preventDefault();
-            
-            var $form = $( this );
-            term = $form.find( 'input[name="s"]' ).val();
-            url = $form.attr( 'action' );
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $("#urlform").serialize(),
-                async: false,
-                success: function(response) { 
-                    console.info("Posted" );
-                    console.info("uuid: " + response.uuid);
-                    
-                    window.location.href =  "/report/"  + response.uuid;                    
-                },
-                error: function(){
-                    console.info("fuck off");                    
-                }
-            });
-        });
-});
-
-
-$(document).ready(function(){
-    $("#login").submit(
-        function(){
-            return login();
-        });
-});
-
 $(document).ready(function() {
-    $("th.hheader").click( function() { showAll() } );
-    
+    $("#submit-url").on("click", function() {
+        console.info("Posting");
+        var form = $(this);
+        term = form.find('input[name="s"]').val();
+        url = "/job";
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $("#url").serialize(),
+            async: false,
+            success: function(response) {
+                console.info("Posted");
+                console.info("uuid: " + response.uuid);
+                window.location.href = "/report/" + response.uuid;
+            },
+            error: function() {
+                console.info("fuck off");
+            }
+        });
+    });
+    $("#login").submit(function() {
+        return login();
+    });
+    $("th.hheader").click(function() {
+        showAll();
+    });
     var lol = 0;
-    
     $.ajaxSetup({
         timeout: 1,
-        retryAfter: 2000
+        retryAfter: 2e3
     });
-    
-    
-    if( $("#report").length > 0) {        
+    if ($("#report").length > 0) {
         checkIfFinished();
     }
-    
     $(".fancybox-thumb").fancybox({
-        prevEffect: 'none',
-        nextEffect: 'none',
-        scrolling: 'true',
+        prevEffect: "none",
+        nextEffect: "none",
+        scrolling: "true",
         helpers: {
             title: {
-                type: 'outside'
+                type: "outside"
             },
             thumbs: {
                 width: 50,
@@ -65,16 +51,12 @@ $(document).ready(function() {
             }
         }
     });
-    
-   
     var active;
     $("div.headers").hide();
-    
-    
-    $("tr.connection").click(function () {
+    $("tr.connection").click(function() {
         var data = $(this).find("div.headers");
         console.info(data);
-        if (!data.is(active) && typeof active !== 'undefined') {
+        if (!data.is(active) && typeof active !== "undefined") {
             active.hide();
         }
         data.toggle();
@@ -82,82 +64,76 @@ $(document).ready(function() {
     });
 });
 
-
-function showAll(){
-    $("tr.connection").each( function( index ) {
+function showAll() {
+    $("tr.connection").each(function(index) {
         var data = $(this).find("div.headers");
         data.toggle();
-    } );
+    });
 }
 
-function checkIfFinished(){
-        $.ajax({
-            url: window.location.href,
-            type: 'GET',
-            async: false,
-            cache: false,
-            contentType: 'application/json',
-            success:function(){
-                console.info("Success");
-                if (failed === true){
-                    failed = false;
-                    window.document.location.reload(true);
-                }                    
-                $.unblockUI();
-            },            
-            error: function(data, textStatus){
-                if(blocked === false){
-                    $.blockUI({ message: '<h1><img src="/static/images/busy.gif" /> Running FjoSpidie...</h1>' });
-                    blocked = true;
-                }
-                failed = true;
-                console.info("Failed: " + textStatus );
-                setTimeout(checkIfFinished, 2000);
+function checkIfFinished() {
+    $.ajax({
+        url: window.location.href,
+        type: "GET",
+        async: false,
+        cache: false,
+        contentType: "application/json",
+        success: function() {
+            console.info("Success");
+            if (failed === true) {
+                failed = false;
+                window.document.location.reload(true);
             }
-        });
+            $.unblockUI();
+        },
+        error: function(data, textStatus) {
+            if (blocked === false) {
+                $.blockUI({
+                    message: '<h1><img src="/static/images/busy.gif" /> Running FjoSpidie...</h1>'
+                });
+                blocked = true;
+            }
+            failed = true;
+            console.info("Failed: " + textStatus);
+            setTimeout(checkIfFinished, 2e3);
+        }
+    });
 }
 
-
-
-function hideAllMessages()
-{
+function hideAllMessages() {
     var messagesHeights;
-    
-    messagesHeights = $('.error').outerHeight(); // fill array
-    $('.error').css('top', -messagesHeights); //move element outside viewport
+    messagesHeights = $(".error").outerHeight();
+    $(".error").css("top", -messagesHeights);
 }
-function showMessage(type)
-{
-    var data  = $("p#errormsg").text();
-    if ( data ){
+
+function showMessage(type) {
+    var data = $("p#errormsg").text();
+    if (data) {
         hideAllMessages();
-        $('.container').animate({"top": "+=80px"});
-        $('.error').animate({top:"0"}, 500);
+        $(".container").animate({
+            top: "+=80px"
+        });
+        $(".error").animate({
+            top: "0"
+        }, 500);
     }
 }
 
-$(document).ready(function(){
-
-    // Initially, hide them all
+$(document).ready(function() {
     hideAllMessages();
-    
-    // Show message
     showMessage();
-
-     // When message is clicked, hide it
-    $('.error').click(function(){
-        $(this).animate({top: -$(this).outerHeight()}, 500);
-        $('.container').animate({"top":"-=80px"}, 500);
-
+    $(".error").click(function() {
+        $(this).animate({
+            top: -$(this).outerHeight()
+        }, 500);
+        $(".container").animate({
+            top: "-=80px"
+        }, 500);
     });
-
-});
-
-
-$(document).ready(function(){
-    $('tr.report').click(function(){
-        $.blockUI({ message: '<h2><img src="/static/images/busy.gif" /> Loading report...</h2>' }); 
-        window.location.assign( $(this).find('td').attr('rel') );
-//        document.location.href = $(this).find('td').attr('rel');
+    $("tr.report").click(function() {
+        $.blockUI({
+            message: '<h2><img src="/static/images/busy.gif" /> Loading report...</h2>'
+        });
+        window.location.assign($(this).find("td").attr("rel"));
     });
 });
