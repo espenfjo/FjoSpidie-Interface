bde2d12d4c0997b92559d838d7ace8818dcff350
Espen Fjellvær Olsen <espen@mrfjo.org>
2014-02-15 15:49:24 +0100

    Fix passing of useragent and referer

--- reports/jobs.py
+++ reports/jobs.py
@@ -5,11 +5,14 @@ from django.utils.encoding import smart_str
 def job(uuid, url, useragent, referer):
     c = docker.Client(base_url='unix://var/run/docker.sock', version="1.7")
     image = "espenfjo/fjospidie:latest"
-    command = "bash -c \"cd /opt/fjospidie;pwd; xvfb-run -a python fjospidie.py --url '{}' --uuid '{}'\"".format(smart_str(url),uuid)
+
+    fjospidie_cmd = "cd /opt/fjospidie;pwd; xvfb-run -a python fjospidie.py --debug --url '{}' --uuid '{}'".format(smart_str(url),uuid)
     if referer:
-        command += " --referer '{}'".format(referer)
+        fjospidie_cmd += " --referer '{}'".format(referer)
     if useragent:
-        command += " --useragent '{}'".format(useragent)
+        fjospidie_cmd += " --useragent '{}'".format(useragent)
+
+    command = "bash -c \"{}\"".format(fjospidie_cmd)
 
     container = c.create_container(image, command=command, volumes={"/mnt/fjospidie":{}}, detach=True)
     c.start(container, binds={'/mnt/fjospidie':'/mnt/fjospidie'}, lxc_conf=None)
